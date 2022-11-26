6d1fa3108d7c625a2aaca8e7b19dd13fb912ca2c
Larixssa <larissasvendetta@gmail.com>
2022-11-26 12:05:37 +0800

    Update Prompt.*

--- src/display/Prompt.cpp
+++ src/display/Prompt.cpp
@@ -1,1 +1,43 @@
 #include "Prompt.h"
+#include "cylib/StringUtils.h"
+#include "cystdio/CyStdio.h"
+
+#include<string>
+
+std::string Prompt::DEFAULT_INPUT_TEXT = "Input String";
+std::string Prompt::DEFAULT_YXCONTENT_TEXT = "Y/X";
+std::string Prompt::DEFAULT_YX_PROMPT_TYPE = "default";
+std::string Prompt::YX_CUSTOM_SURROUND_L = "[";
+std::string Prompt::YX_CUSTOM_SURROUND_R = "]";
+
+std::string createInputPrompt(std::string inputText)
+{
+	std::string fInputText;
+	if (!inputText.empty())
+	{
+		fInputText = "[" + inputText + "] > ";
+	}
+	return fInputText;
+}
+
+std::string Prompt::createYxOptionPrompt(std::string yxContent, std::string promptType)
+{
+	if (!yxContent.empty() && !promptType.empty())
+	{
+		if (StringUtils::strCompare(promptType, "default")) {
+			CyStdio::cPut(StringUtils::surroundString(yxContent, "(", ")"));
+		} else if (StringUtils::strCompare(promptType, "custom")) {
+			CyStdio::cPut(StringUtils::surroundString(yxContent, Prompt::YX_CUSTOM_SURROUND_L, Prompt::YX_CUSTOM_SURROUND_R));
+		}
+	}
+}
+
+void Prompt::displayInputPrompt()
+{
+	CyStdio::cPut(Prompt::createInputPrompt(Prompt::DEFAULT_INPUT_TEXT));
+}
+
+void Prompt::displayYxOptionPrompt()
+{
+	CyStdio::cPut(Prompt::createYxOptionPrompt(Prompt::DEFAULT_YXCONTENT_TEXT, Prompt::DEFAULT_YX_PROMPT_TYPE));
+}
--- src/display/Prompt.h
+++ src/display/Prompt.h
@@ -8,9 +8,12 @@ class Prompt
 public:
 	static std::string DEFAULT_INPUT_TEXT;
 	static std::string DEFAULT_YXCONTENT_TEXT;
+	static std::string DEFAULT_YX_PROMPT_TYPE;
+	static std::string YX_CUSTOM_SURROUND_L;
+	static std::string YX_CUSTOM_SURROUND_R;
 	static std::string createInputPrompt(std::string inputText);
+	static std::string createYxOptionPrompt(std::string yxContent, std::string promptType);
 	static void displayInputPrompt();
-	static std::string createYxOptionPrompt(std::string yxContent);
 	static void displayYxOptionPrompt();
 };
 
