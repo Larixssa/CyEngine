d36b2b0346a91e4388377fe703b65c11c2f378f2
Larixssa <larissasvendetta@gmail.com>
2022-11-26 11:53:48 +0800

    Update prompt.*

--- src/display/Prompt.h
+++ src/display/Prompt.h
@@ -1,4 +1,17 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
+#include<string>
+
+class Prompt
+{
+public:
+	static std::string DEFAULT_INPUT_TEXT;
+	static std::string DEFAULT_YXCONTENT_TEXT;
+	static std::string createInputPrompt(std::string inputText);
+	static void displayInputPrompt();
+	static std::string createYxOptionPrompt(std::string yxContent);
+	static void displayYxOptionPrompt();
+};
+
 #endif // Prompt.h
