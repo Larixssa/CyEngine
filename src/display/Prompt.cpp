58e405f9a62661c544c9e35d61637cd13f0dbc2a
Larixssa <larissasvendetta@gmail.com>
2022-11-26 15:00:05 +0800

    New createShellPrompt function to Prompt.

--- src/display/Prompt.cpp
+++ src/display/Prompt.cpp
@@ -9,8 +9,9 @@ std::string Prompt::DEFAULT_YXCONTENT_TEXT = "Y/X";
 std::string Prompt::DEFAULT_YX_PROMPT_TYPE = "default";
 std::string Prompt::YX_CUSTOM_SURROUND_L = "[";
 std::string Prompt::YX_CUSTOM_SURROUND_R = "]";
+std::string Prompt::DEFAULT_SHELL_PROMPT_TEXT = "usr/console";
 
-std::string createInputPrompt(std::string inputText)
+std::string Prompt::createInputPrompt(std::string inputText)
 {
 	std::string fInputText;
 	if (!inputText.empty())
@@ -22,14 +23,26 @@ std::string createInputPrompt(std::string inputText)
 
 std::string Prompt::createYxOptionPrompt(std::string yxContent, std::string promptType)
 {
+	std::string fPromptString;
 	if (!yxContent.empty() && !promptType.empty())
 	{
 		if (StringUtils::strCompare(promptType, "default")) {
-			CyStdio::cPut(StringUtils::surroundString(yxContent, "(", ")"));
+			fPromptString = StringUtils::surroundString(yxContent, "(", ")");
 		} else if (StringUtils::strCompare(promptType, "custom")) {
-			CyStdio::cPut(StringUtils::surroundString(yxContent, Prompt::YX_CUSTOM_SURROUND_L, Prompt::YX_CUSTOM_SURROUND_R));
+			fPromptString = StringUtils::surroundString(yxContent, Prompt::YX_CUSTOM_SURROUND_L, Prompt::YX_CUSTOM_SURROUND_R);
 		}
 	}
+	return fPromptString;
+}
+
+std::string Prompt::createShellPrompt(std::string shellPromptText)
+{
+	std::string fShellPromptText;
+	if (!shellPromptText.empty())
+	{
+		fShellPromptText = shellPromptText + "\n> ";
+	}
+	return fShellPromptText;
 }
 
 void Prompt::displayInputPrompt()
@@ -40,4 +53,9 @@ void Prompt::displayInputPrompt()
 void Prompt::displayYxOptionPrompt()
 {
 	CyStdio::cPut(Prompt::createYxOptionPrompt(Prompt::DEFAULT_YXCONTENT_TEXT, Prompt::DEFAULT_YX_PROMPT_TYPE));
+}
+
+void Prompt::displayShellPrompt()
+{
+	CyStdio::cPut(Prompt::createShellPrompt(Prompt::DEFAULT_SHELL_PROMPT_TEXT));
 }
--- src/display/Prompt.h
+++ src/display/Prompt.h
@@ -11,10 +11,13 @@ public:
 	static std::string DEFAULT_YX_PROMPT_TYPE;
 	static std::string YX_CUSTOM_SURROUND_L;
 	static std::string YX_CUSTOM_SURROUND_R;
+	static std::string DEFAULT_SHELL_PROMPT_TEXT;
 	static std::string createInputPrompt(std::string inputText);
 	static std::string createYxOptionPrompt(std::string yxContent, std::string promptType);
+	static std::string createShellPrompt(std::string shellPromptText);
 	static void displayInputPrompt();
 	static void displayYxOptionPrompt();
+	static void displayShellPrompt();
 };
 
 #endif // Prompt.h
