1db5655845b8441c5333c88bc0dbe96a7f8a9c78
Larixssa <larissasvendetta@gmail.com>
2022-11-30 12:07:19 +0800

    Update CommandHandler

--- src/commandutils/CommandHandler.cpp
+++ src/commandutils/CommandHandler.cpp
@@ -1,6 +1,9 @@
 #include "CommandHandler.h"
 #include "Command.h"
 #include "cystdio/CyStdio.h"
+#include "cylib/StringUtils.h"
+#include "parser/sources/CommandCalls.h"
+#include "init/Init.h"
 
 #include<string>
 
@@ -8,5 +11,24 @@ void CommandHandler::pushCommands(std::string* commandsList, std::string* comman
 {
 	for (int i = 0; i < commandListsSize; i++) {
 		CyStdio::cPut(Command::createCommand(commandsList[i], commandsDesc[i]) + "\n");
+	}
+}
+
+void CommandHandler::parseCommand(std::string* commandsList, std::string cmdIO, int cmdcSize)
+{
+	bool doParse = false;
+
+	for (int i = 0; i < cmdcSize; ++i)
+	{
+		if (StringUtils::strCompare(cmdIO, commandsList[i]))
+		{
+			doParse = true;
+		}
+	}
+
+	if (doParse) {
+		CommandCalls::callCommandParser(cmdIO);
+	} else {
+		Init::initClient(false, false, false);
 	}
 }
